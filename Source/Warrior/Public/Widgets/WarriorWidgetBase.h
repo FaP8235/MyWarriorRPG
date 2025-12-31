9cf0c69106c4a30f45c133c003860c4d1cca70be
wsy <1270488040@qq.com>
2025-12-31 15:57:39 +0800

    为所有角色配置血条、怒气（英雄角色）改变事件广播和监听器

---
+++ Source/Warrior/Public/Widgets/WarriorWidgetBase.h
@@ -0,0 +1,24 @@
+// FaP All Rights Reserve
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WarriorWidgetBase.generated.h"
+
+
+class UHeroUIComponent;
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeOnInitialized() override;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "OnOwningHeroUIComponentInitialized"))
+	void BP_OnOwningHeroUIComponentInitialized(UHeroUIComponent* OwningHeroUIComponent);
+};
