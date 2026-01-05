6343607694567dd7d7ed5cab4a27adc6253a433e
wsy <1270488040@qq.com>
2026-01-05 15:01:08 +0800

    修复敌人检测目标逻辑，使用EQS实现敌人包围玩家

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -36,7 +36,7 @@ ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& O
 	
 	const IGenericTeamAgentInterface* OtherTeamAgent = Cast<const IGenericTeamAgentInterface>(PawnToCheck->GetController());
 
-	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() != GetGenericTeamId())
+	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() < GetGenericTeamId())
 	{
 		return ETeamAttitude::Hostile;
 	}
@@ -71,12 +71,23 @@ void AWarriorAIController::BeginPlay()
 
 void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	if (Stimulus.WasSuccessfullySensed() && Actor)
+	if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
 	{
-		/*Debug::Print(Actor->GetActorNameOrLabel() + TEXT(" was sensed"), FColor::Green);*/
-		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		if (!BlackboardComponent->GetValueAsObject(FName("TargetActor")))
 		{
-			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			if (Stimulus.WasSuccessfullySensed() && Actor)
+			{
+				BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			}
 		}
 	}
+
+	//if (Stimulus.WasSuccessfullySensed() && Actor)
+	//{
+	//	/*Debug::Print(Actor->GetActorNameOrLabel() + TEXT(" was sensed"), FColor::Green);*/
+	//	if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+	//	{
+	//		BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+	//	}
+	//}
 }
