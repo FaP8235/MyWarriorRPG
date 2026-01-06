3c00e55b5e360bcc39467b6cce411b589e7bea78
wsy <1270488040@qq.com>
2026-01-06 13:52:04 +0800

    将敌人攻击通过事件传递给主角

--- Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,12 +2,50 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
+
 #include "WarriorDebugHelper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	/*if (HitActor)
 	{
 		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+	}*/
+	if (OverlappedActors.Contains(HitActor))
+	{
+		return;
+	}
+	OverlappedActors.AddUnique(HitActor);
+
+	// TODO: Implement block check
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBloking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+
+
+	if (bIsPlayerBloking && !bIsMyAttackUnblockable)
+	{
+		// TODO: check if the block is valid
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	if (bIsValidBlock)
+	{
+		// TODO: Handle successful block
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
