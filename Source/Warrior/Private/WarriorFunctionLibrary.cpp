4b5c49a1215bca63ab897f95809f1da58b06db4f
wsy <1270488040@qq.com>
2026-01-12 15:27:06 +0800

    配置角色受伤能力逻辑、动画及音效

--- Source/Warrior/Private/WarriorFunctionLibrary.cpp
+++ Source/Warrior/Private/WarriorFunctionLibrary.cpp
@@ -6,6 +6,8 @@
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "Interfaces/PawnCombatInterface.h"
 #include "GenericTeamAgentInterface.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "WarriorGameplayTags.h"
 
 UWarriorAbilitySystemComponent* UWarriorFunctionLibrary::NativeGetWarriorASCFromActor(AActor* InActor)
 {
@@ -86,4 +88,41 @@ bool UWarriorFunctionLibrary::IsTargetPawnHostile(APawn* QueryPawn, APawn* Targe
 float UWarriorFunctionLibrary::GetScalableFloatValueAtLevel(const FScalableFloat& InScalableFloat, float InLevel)
 {
     return InScalableFloat.GetValueAtLevel(InLevel);
+}
+
+FGameplayTag UWarriorFunctionLibrary::ComputeHitReactDirectionTag(AActor* InAttacker, AActor* InVictim, float& OutAngleDifference)
+{
+    check(InAttacker && InVictim);
+
+    const FVector VictimForward = InVictim->GetActorForwardVector();
+    const FVector VictimAttackerNormalized = (InAttacker->GetActorLocation() - InVictim->GetActorLocation()).GetSafeNormal();
+
+    const float DotResult = FVector::DotProduct(VictimForward, VictimAttackerNormalized);
+    OutAngleDifference = UKismetMathLibrary::DegAcos(DotResult);
+
+    const FVector CrossResult = FVector::CrossProduct(VictimForward, VictimAttackerNormalized);
+
+    if (CrossResult.Z < 0.f)
+    {
+        OutAngleDifference *= -1.f;
+    }
+
+    if (OutAngleDifference >= -45.f && OutAngleDifference <= 45.f)
+    {
+        return WarriorGameplayTags::Shared_Status_HitReact_Front;
+    }
+    else if (OutAngleDifference < -45.f && OutAngleDifference >= -135.f)
+    {
+        return WarriorGameplayTags::Shared_Status_HitReact_Left;
+    }
+    else if (OutAngleDifference < -135.f || OutAngleDifference > 135.f)
+    {
+        return WarriorGameplayTags::Shared_Status_HitReact_Back;
+    }
+    else if (OutAngleDifference > 45.f && OutAngleDifference <= 135.f)
+    {
+        return WarriorGameplayTags::Shared_Status_HitReact_Right;
+    }
+
+    return WarriorGameplayTags::Shared_Status_HitReact_Front;
 }
