d602f3b69af18b984e058cebe40a4de160c62253
wsy <1270488040@qq.com>
2026-01-15 15:21:52 +0800

    配置Boss角色死亡能力以及死亡后移除角色面板

--- Source/Warrior/Public/Components/UI/EnemyUIComponent.h
+++ Source/Warrior/Public/Components/UI/EnemyUIComponent.h
@@ -6,6 +6,7 @@
 #include "Components/UI/HeroUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
+class UWarriorWidgetBase;
 /**
  * 
  */
@@ -14,4 +15,14 @@ class WARRIOR_API UEnemyUIComponent : public UHeroUIComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
+
+private:
+	TArray<UWarriorWidgetBase*> EnemyDrawnWidgets;
+
 };
